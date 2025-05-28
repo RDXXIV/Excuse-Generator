b0b5a2a1ed68f68031d2cc78e317fe5ef7ed1ba6
Rudy Ducatel <109167509+RDXXIV@users.noreply.github.com>
2025-05-28 21:28:17 +0000

    added files to the repo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird', 'bobby'];
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
 let action = ['ate', 'peed', 'crushed', 'broke'];
 let what = ['my homework', 'my phone', 'the car'];
 let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
